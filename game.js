b2f41fa8ff1df78bac5fecb9348603f5d1e205fc
Aaron Sala <salaaron2@gmail.com>
2024-03-11 13:56:44 +0300

    fully working game

--- game.js
+++ game.js
@@ -3,21 +3,52 @@ const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const dice = document.querySelector(".dice");
 const rollDice = document.querySelector(".btn--roll");
-//const newGame = document.querySelector(".btn--new");
 const diceRoll = document.querySelectorAll(".dice i");
-const player0 = document.querySelector(".player1");
-const player1 = document.querySelector(".player2");
+const player0 = document.querySelector(".player0");
+const player1 = document.querySelector(".player1");
 const current1 = document.getElementById("current--0");
+const current2 = document.getElementById("current--1");
 const hold = document.querySelector(".btn--hold");
+const newGame = document.querySelector(".btn--new");
+const name1 = document.querySelector('#name--0')
+ const name2 = document.querySelector("#name--1");
 
 //initialize the global values to 0
 
-score0.textContent = 0;
-score1.textContent = 0;
 
-let scores = [0, 0];
-let current = 0;
-let activePlayer = 0;
+let scores  ;
+let current ;
+let activePlayer ;
+
+function general() {
+  name1.textContent = "PLAYER 1";
+  name2.textContent = "PLAYER 2";
+  name1.style.color = "#fff";
+  name2.style.color = "#fff";
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current1.textContent = 0;
+  current2.textContent = 0;
+  player0.classList.remove("player--winner");
+  player1.classList.remove("player--winner");
+  player0.classList.add("player--active");
+  player1.classList.remove("player--active");
+  dice.classList.add("hidden");
+  rollDice.classList.remove("hidden");
+  hold.classList.remove("hidden");
+  scores = [0, 0];
+  current = 0;
+  activePlayer = 0;
+}
+general()
+//switching players
+function switchplayer() {
+  current = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = current;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
+}
 //impliment the event of roling dice
 rollDice.addEventListener("click", () => {
   dice.classList.remove("hidden");
@@ -34,20 +65,38 @@ rollDice.addEventListener("click", () => {
     current += randomIndex + 1;
     document.getElementById(`current--${activePlayer}`).textContent = current;
   } else {
-    current = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = current;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0.classList.toggle("player--active");
-    player1.classList.toggle("player--active");
+    switchplayer();
   }
 });
 
 //implimenting the hhold event for the  button hold
-hold.addEventListener("click", holdpoints = () => {
+hold.addEventListener(
+  "click",
+  (holdpoints = () => {
     scores[activePlayer] += current;
-    document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer]
-})
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-// newGame.addEventListener("click", () => {
-//   dice.classList.add("hidden");
-// });
+    //checking if score is 100
+    if (scores[activePlayer] >= 20) {
+      //finish the game
+      document
+        .querySelector(`.player${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player${activePlayer}`)
+        .classList.remove("player--active");
+      let winner = document.getElementById(`name--${activePlayer}`);
+      winner.textContent = "WINNER!!!";
+      winner.style.color = "red";
+      //remove the dice if winner found
+      dice.classList.add("hidden");
+      rollDice.classList.add("hidden");
+      hold.classList.add("hidden");
+    } else {
+      switchplayer();
+    }
+  })
+);
+//starting a new game
+newGame.addEventListener("click", general);
